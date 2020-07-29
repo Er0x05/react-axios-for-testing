b4a9095ec156fe27b8ffcdeeff691bdbcc864f26
Pek Leung <error@Peks-MacBook-Pro.local>
2020-07-29 17:35:38 +0800

    add get/post/delete/interceptors

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
@@ -6,20 +7,58 @@ import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
 
 class Blog extends Component {
+    state = {
+        posts: [],
+        selectId: null,
+        errStatus: false
+    };
+        
+    componentDidMount(){
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+            .then(res=>{
+                const posts = res.data.slice(0,4).map(post=>({...post,author: 'Max'}))
+                this.setState({ posts: posts });
+            })
+            .catch(err=>{
+                console.log(err)
+                this.setState({ errStatus: true })
+            })
+    }
+
+    changeSelectedId = (id) =>{
+        this.setState({ selectId: id })
+    }
+
     render () {
-        return (
-            <div>
+        const posts = this.state.posts.map( post=> {
+            return <Post 
+                key={post.id} 
+                title={post.title} 
+                author={post.author} 
+                clicked={()=>this.changeSelectedId(post.id)} />;
+        })
+        let sections = null;
+        if (!this.state.errStatus){
+            sections = <div>
                 <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
+                    {posts}
                 </section>
                 <section>
-                    <FullPost />
+                    <FullPost id={this.state.selectId} />
                 </section>
                 <section>
                     <NewPost />
                 </section>
+            </div>
+        }
+
+        if (this.state.errStatus){
+            sections = <p style={{textAlign: 'center'}}>Got Error!</p>
+        }
+        
+        return (
+            <div>
+                {sections}
             </div>
         );
     }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Axios from 'axios';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+
+const interceptor = Axios.interceptors.request.use( request =>{
+    console.log( request );
+    return request
+}, error => {
+    console.log( error );
+    return Promise.reject(error);
+});
+
+// Axios.interceptors.request.eject(interceptor)
+
+
+Axios.interceptors.response.use( response => {
+    console.log(response);
+    return response;
+},error => {
+    console.log(error);
+    return Promise.reject(error);
+}
+)
 
 ReactDOM.render( <App />, document.getElementById( 'root' ) );
 registerServiceWorker();
